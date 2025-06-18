c1cab2f9b10cae83fd141b7ff131db449784e972
Loop1397 <shwlsdn0906@gmail.com>
2025-06-18 22:10:03 +0900

    style: css 초기화
    필요없는 문서 삭제 및 css reset

---
+++ src/pages/Index.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function Index() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Index
