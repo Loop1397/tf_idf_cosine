ed86667a243d7fafd82d19feead224f0a71b1ae5
Loop1397 <shwlsdn0906@gmail.com>
2025-07-03 01:05:00 +0900

    feat: 코사인 유사도 구하는 메소드 완성
    - 벡터 내적을 구하는 calculateDotProduct메소드 작성
    - calculateNorm과 calculateDotProduct을 이용해 코사인 유사도를 구하는 calculateCosineSimilarity 작성

--- src/utils/similarity.ts
+++ src/utils/similarity.ts
@@ -1,13 +1,29 @@
 class Similarity {
     constructor() { }
 
+    // ベクトルのノルムを求めるメッソド
     private calculateNorm = (vector: number[]) => {
-        const result = vector.reduce((acc, current) => acc + (current ** 2), 0)
+        const result = vector.reduce((acc, cur) => acc + (cur ** 2), 0)
         return Math.sqrt(result);
     }
 
-    public calculateCosineSimilarity = (vector1: number[]) => {
-        console.log(this.calculateNorm(vector1));
+    // ベクトルの内積を求めるメッソド
+    private calculateDotProduct = (vector1: number[], vector2: number[]) => {
+        return vector1.reduce((acc, cur, index) => acc + (cur * vector2[index]), 0);
+    }
+
+    // 二つのベクトルのコサイン類似度を求めるメッソド
+    public calculateCosineSimilarity = (vector1: number[], vector2: number[]) => {
+        // vector1とvector2の次元が違うときのエラー
+        if (vector1.length !== vector2.length) {
+            throw new Error('vector1とvector2の長さが違います！');
+        }
+        // vectorの中身がないときのエラー
+        else if (vector1.length === 0) {
+            throw new Error('空のベクトルが入っています！');
+        }
+
+        return this.calculateDotProduct(vector1, vector2) / (this.calculateNorm(vector1) * this.calculateNorm(vector2))
     }
 }
 
